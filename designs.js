5a75b9728612c8d6195d430570c16ecffa92fbd0
McBKorir <34438978+ipkipkorir@users.noreply.github.com>
2018-06-30 17:59:56 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,14 +3,14 @@
 
 // When size is submitted by the user, call makeGrid()
 
+
 function makeGrid() {
   var height = $('inputHeight').val();
   var width = $('inputWeight').val();
   
   while(var i = 1; i <= height; i++)
   {
-      $('table').append('<tr></tr>');
-      
+      $('#pixelCanvas').append('<tr></tr>');
       for(var j = 1; j <= width; j++)
       {
         $('tr:last').append('<td></td>');
@@ -18,5 +18,4 @@ function makeGrid() {
   }
   
   // Your code goes here!
-
 }
