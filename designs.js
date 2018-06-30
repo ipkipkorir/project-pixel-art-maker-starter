677e53a9d6ad86188ae2a173e9828d0781c23275
McBKorir <34438978+ipkipkorir@users.noreply.github.com>
2018-06-30 17:38:28 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -7,6 +7,15 @@ function makeGrid() {
   var height = $('inputHeight').val();
   var width = $('inputWeight').val();
   
+  while(var i = 1; i <= height; i++)
+  {
+      $('table').append('<tr></tr>');
+      
+      for(var j = 1; j <= width; j++)
+      {
+        $('tr:last').append('<td></td>');
+      }
+  }
   
   // Your code goes here!
 
