4e297efbad65ed48de484627981f10181786115c
McBKorir <34438978+ipkipkorir@users.noreply.github.com>
2018-06-30 18:32:28 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,22 +1,24 @@
 // Select color input
 // Select size input
   var color = $('colorPicker').val();
-  var height = $('inputHeight').val();
-  var width = $('inputWeight').val();
-  
+    $('sizePicker').submit(function (event) {
+    event.preventDefault();
+    var height = $('inputHeight').val();
+    var width = $('inputWeight').val();
+    makeGrid(height, width);
+});
+
 // When size is submitted by the user, call makeGrid()
-$('sizePicker').submit();
 
-function makeGrid() {
-
-  while(var i = 1; i <= height; i++)
+function makeGrid(n, m) 
+{ 
+  while(var i = 1; i <= n; i++)
   {
       $('#pixelCanvas').append('<tr></tr>');
-      for(var j = 1; j <= width; j++)
+      for(var j = 1; j <= m; j++)
       {
         $('tr:last').append('<td></td>');
       }
   }
-  
-  // Your code goes here!
 }
+
