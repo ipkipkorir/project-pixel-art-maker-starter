65c9dfc9c26e732265ec9afcebfd8c7dbd1b2260
McBKorir <34438978+ipkipkorir@users.noreply.github.com>
2018-06-30 18:46:17 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,14 +1,23 @@
 // Select color input
 // Select size input
-var height = $('inputHeight').val();
-var width = $('inputWeight').val();  
-var color = $('colorPicker').val();
  
 $('sizePicker').submit(function (event) {
     event.preventDefault();
+    var height = $('inputHeight').val();
+    var width = $('inputWeight').val();  
     makeGrid(height, width);
 });
-
+$('td').click(function addColor() 
+              {
+ var color = $('colorPicker').val();
+ if(color.attr('style'))
+ {
+  color.removeAtrr('style');
+ }
+ else{
+  color.attr('style', 'background-color:' + color);
+ }
+});
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid(n, m) 
